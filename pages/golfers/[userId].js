4b2fe03a5c67bedd46ca58b9f0554e0aea225968
eusebiucarhat <eusebiu.carhat@golfgenius.com>
2022-05-25 14:07:55 +0300

    create profile page for golfers

---
+++ pages/golfers/[userId].js
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
+import ScoreCard from '../../components/ScoreCard'
+import useUserScores from '../../lib/useUserScores'
+
+const GolferProfile = () => {
+  const router = useRouter()
+  const { userId } = router.query
+
+  const { userScores, error } = useUserScores(userId)
+
+  return (
+    <Layout>
+      <>
+        { error ?
+          <h2>{error}</h2>
+        : (
+          <div>
+            {userScores?.name && <h1>{userScores.name}'s scores</h1>}
+            {userScores?.scores?.map(score => (
+              <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={score.user_id}
+                userName={userScores.name}
+              />
+            ))}
+          </div>
+        )}
+      </>
+    </Layout>
+  )
+}
+
+export default GolferProfile
